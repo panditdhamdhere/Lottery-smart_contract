d9efdf9de1fa3c42912202f6fdc971873d7f3888
panditdhamdhere <dhamdherepandit@gmail.com>
2023-06-30 18:25:03 +0530

    all error fixed in helperconfig

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -15,8 +15,11 @@ contract HelperConfig is Script {
         uint64 subscriptionId;
         uint32 callbackGasLimit;
         address link;
+        uint256 deployKey;
     }
 
+    uint256 public constant DEFAULT_ANVIL_KEY =
+        0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
     NetworkConfig public activeNetworkConfig;
 
     constructor() {
@@ -27,7 +30,7 @@ contract HelperConfig is Script {
         }
     }
 
-    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+    function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
         return
             NetworkConfig({
                 entranceFee: 0.01 ether,
@@ -36,7 +39,8 @@ contract HelperConfig is Script {
                 gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
                 subscriptionId: 0,
                 callbackGasLimit: 500000,
-                link: 0x779877A7B0D9E8603169DdbD7836e478b4624789
+                link: 0x779877A7B0D9E8603169DdbD7836e478b4624789,
+                deployKey: vm.envUint("PRIVATE_KEY")
             });
     }
 
@@ -64,7 +68,8 @@ contract HelperConfig is Script {
                 gasLane: 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c,
                 subscriptionId: 1893,
                 callbackGasLimit: 500000,
-                link: address(link)
+                link: address(link),
+                deployKey: DEFAULT_ANVIL_KEY
             });
     }
 }
