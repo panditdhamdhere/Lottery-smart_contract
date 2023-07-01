9e81d69886d3b2673103b50d3ce67bdc84f31d67
panditdhamdhere <dhamdherepandit@gmail.com>
2023-07-01 15:11:20 +0530

    learned console debugging

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -25,6 +25,7 @@ pragma solidity ^0.8.19;
 
 import {VRFCoordinatorV2Interface} from "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import {console} from "forge-std/console.sol";
 
 contract Raffle is VRFConsumerBaseV2 {
     // error
--- test/integration/InteractionsTest.t.sol
+++ test/integration/InteractionsTest.t.sol
@@ -7,7 +7,6 @@
 
 
 
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
 
-contract 
+
+
